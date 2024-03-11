9bb9830e487bece5a72e10bbfdfb6f41b86d8a0d
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-11 18:21:53 +0330

    create open/close modal function

--- src/main.js
+++ src/main.js
@@ -4,3 +4,11 @@ jalaliDatepicker.startWatch();
 // date.addEventListener("change", () => {
 //   alert(date.value);
 // });
+
+// open or close modals
+function closeModal(item) {
+  item.classList.add("invisible");
+}
+function openModal(item) {
+  item.classList.remove("invisible");
+}
